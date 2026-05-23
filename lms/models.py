d4eb72adaba238e4d13d4051a784c4768f2093d8
Маргарита Буршева <mbursheva@mail.ru>
2026-05-23 16:36:05 +0300

    Update lms/admin.py, models.py, urls.py, views.py

--- lms/models.py
+++ lms/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Название курса", help_text="Введите название курса")
+    preview = models.ImageField(
+        upload_to="lms/previews",
+        verbose_name="Превью (картинка)",
+        blank=True,
+        null=True,
+        help_text="Загрузите превью(картинку",
+    )
+    description = models.TextField(blank=True, null=True, verbose_name="Описание курса", help_text="Введите описание")
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+        ordering = ["title"]
+
+    def __str__(self):
+        return self.title
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=255, verbose_name="Название урока", help_text="Введите название урока")
+    description = models.TextField(verbose_name="Описание урока", blank=True, null=True, help_text="Введите описание")
+    preview = models.ImageField(
+        upload_to="lms/previews",
+        verbose_name="Превью (картинка)",
+        blank=True,
+        null=True,
+        help_text="Загрузите превью(картинку",
+    )
+    video_url = models.URLField(verbose_name="Ссылка на видео", blank=True, null=True)
+
+    # Связь с курсом: один курс — много уроков
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="lessons", verbose_name="Курс")
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
+        ordering = ["title"]
+
+    def __str__(self):
+        return f"{self.course.title} — {self.title}"
