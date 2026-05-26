c9d0e7fe6f21c9eb95192916372c1923caed2ce8
Маргарита Буршева <mbursheva@mail.ru>
2026-05-26 18:18:24 +0300

    Update users/models.py

--- users/models.py
+++ users/models.py
@@ -45,3 +45,33 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+
+class Payment(models.Model):
+    PAYMENT_METHODS = (
+        ('cash', 'Наличные'),
+        ('transfer', 'Перевод на счет'),
+    )
+    user = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name='payments',
+        verbose_name='Пользователь'
+    )
+    payment_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата оплаты')
+    course = models.ForeignKey(
+        'lms.Course', on_delete=models.CASCADE, null=True, blank=True,
+        verbose_name='Оплаченный курс'
+    )
+    lesson = models.ForeignKey(
+        'lms.Lesson', on_delete=models.CASCADE, null=True, blank=True,
+        verbose_name='Оплаченный урок'
+    )
+    amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Сумма оплаты')
+    payment_method = models.CharField(max_length=10, choices=PAYMENT_METHODS, verbose_name='Способ оплаты')
+
+    class Meta:
+        verbose_name = 'Платеж'
+        verbose_name_plural = 'Платежи'
+        ordering = ['-payment_date']
+
+    def __str__(self):
+        return f'{self.user.email} - {self.amount} ({self.payment_date})'
