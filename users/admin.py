6bb4fe6f4a9027bfd9a2d8e2dc6a05e2e10bf24b
Маргарита Буршева <mbursheva@mail.ru>
2026-05-26 18:17:57 +0300

    Update users/admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,13 +1,32 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.models import Group
-from .models import CustomUser
-
+from django.utils.translation import gettext_lazy as _
+from .models import CustomUser, Payment
 
 @admin.register(CustomUser)
 class CustomUserAdmin(BaseUserAdmin):
-    # Поля, отображаемые в списке пользователей
     list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_active')
     list_filter = ('is_staff', 'is_active')
     search_fields = ('email', 'first_name', 'last_name')
     ordering = ('email',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'phone', 'city', 'avatar')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'first_name', 'last_name', 'phone', 'city', 'avatar', 'is_staff', 'is_active'),
+        }),
+    )
+    readonly_fields = ('last_login',)
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'amount', 'payment_method', 'payment_date', 'course', 'lesson')
+    list_filter = ('payment_method', 'payment_date')
+    search_fields = ('user__email',)
