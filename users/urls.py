a7482f0872b9d5c88e4761e1addfbf4e50472c91
Маргарита Буршева <mbursheva@mail.ru>
2026-05-26 18:19:16 +0300

    Update users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
 from .apps import UsersConfig
-from .views import UserProfileAPIView
-
+from .views import UserProfileAPIView, PaymentListAPIView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path('profile/<int:pk>/', UserProfileAPIView.as_view(), name='user_profile'),
+    path('payments/', PaymentListAPIView.as_view(), name='payment_list'),
 ]
