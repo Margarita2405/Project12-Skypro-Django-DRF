cf4d45d61aaadd6de01e4ba4ec3552e6393ff0cc
Маргарита Буршева <mbursheva@mail.ru>
2026-05-26 18:18:50 +0300

    Update users/serializers.py

--- users/serializers.py
+++ users/serializers.py
@@ -1,10 +1,24 @@
 from rest_framework import serializers
 
-from users.models import CustomUser
+from users.models import CustomUser, Payment
+
+
+class PaymentBriefSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = ['id', 'payment_date', 'amount', 'payment_method', 'course', 'lesson']
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
+    payments = PaymentBriefSerializer(many=True, read_only=True)
+
     class Meta:
         model = CustomUser
-        fields = ['id', 'email', 'first_name', 'last_name', 'phone', 'city', 'avatar']
-        read_only_fields = ['email']  # чтобы email нельзя было изменить через профиль
+        fields = ['id', 'email', 'first_name', 'last_name', 'phone', 'city', 'avatar', 'payments']
+        read_only_fields = ['email'] # чтобы email нельзя было изменить через профиль
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = '__all__'
