18a84b52a3449f571435b759568555dc2da0f26e
Маргарита Буршева <mbursheva@mail.ru>
2026-05-26 18:19:41 +0300

    Update users/views.py

--- users/views.py
+++ users/views.py
@@ -1,8 +1,20 @@
-from rest_framework import generics
-from .models import CustomUser
-from .serializers import UserProfileSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics, filters
+
+from .filters import PaymentFilter
+from .models import CustomUser,Payment
+from .serializers import UserProfileSerializer, PaymentSerializer
 
 
 class UserProfileAPIView(generics.RetrieveUpdateAPIView):
     serializer_class = UserProfileSerializer
     queryset = CustomUser.objects.all()
+
+
+class PaymentListAPIView(generics.ListAPIView):
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_class = PaymentFilter
+    ordering_fields = ['payment_date']
+    ordering = ['-payment_date'] # по умолчанию сначала новые
