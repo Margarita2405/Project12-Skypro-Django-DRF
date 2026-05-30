39011afa667a75124ce0413877a607e0adc6f963
Маргарита Буршева <mbursheva@mail.ru>
2026-05-30 17:28:59 +0300

    Update lms/models.py

--- lms/models.py
+++ lms/models.py
@@ -61,3 +61,20 @@ class Lesson(models.Model):
 
     def __str__(self):
         return f"{self.course.title} — {self.title}"
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="subscriptions", verbose_name="Пользователь"
+    )
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="subscriptions", verbose_name="Курс")
+    subscribed_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата подписки")
+
+    class Meta:
+        verbose_name = "Подписка"
+        verbose_name_plural = "Подписки"
+        # Защита от дублирующихся подписок
+        unique_together = ("user", "course")
+
+    def __str__(self):
+        return f"{self.user} подписан на {self.course}"
