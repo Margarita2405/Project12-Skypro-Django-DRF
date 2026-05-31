bdeb484c04a115cb22efd45909120f45e7ef9597
Маргарита Буршева <mbursheva@mail.ru>
2026-05-31 22:00:10 +0300

    Update users/models.py,admin.py,urls.py,views.py

--- users/admin.py
+++ users/admin.py
@@ -1,32 +1,49 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
+
 from .models import CustomUser, Payment
+
 
 @admin.register(CustomUser)
 class CustomUserAdmin(BaseUserAdmin):
-    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_active')
-    list_filter = ('is_staff', 'is_active')
-    search_fields = ('email', 'first_name', 'last_name')
-    ordering = ('email',)
+    list_display = ("email", "first_name", "last_name", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_active")
+    search_fields = ("email", "first_name", "last_name")
+    ordering = ("email",)
 
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        (_('Personal info'), {'fields': ('first_name', 'last_name', 'phone', 'city', 'avatar')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        (_('Important dates'), {'fields': ('last_login',)}),
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "phone", "city", "avatar")}),
+        (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        (_("Important dates"), {"fields": ("last_login",)}),
     )
 
     add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'first_name', 'last_name', 'phone', 'city', 'avatar', 'is_staff', 'is_active'),
-        }),
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "first_name",
+                    "last_name",
+                    "phone",
+                    "city",
+                    "avatar",
+                    "is_staff",
+                    "is_active",
+                ),
+            },
+        ),
     )
-    readonly_fields = ('last_login',)
+    readonly_fields = ("last_login",)
+
 
 @admin.register(Payment)
 class PaymentAdmin(admin.ModelAdmin):
-    list_display = ('user', 'amount', 'payment_method', 'payment_date', 'course', 'lesson')
-    list_filter = ('payment_method', 'payment_date')
-    search_fields = ('user__email',)
+    list_display = ("user", "amount", "payment_method", "payment_date", "course", "lesson", "status", "payment_link")
+    list_filter = ("payment_method", "payment_date", "status")
+    search_fields = ("user__email",)
--- users/urls.py
+++ users/urls.py
@@ -1,18 +1,15 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from .apps import UsersConfig
-from .views import UserProfileAPIView, PaymentListAPIView, UserCreateAPIView
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from .views import PaymentListAPIView, UserCreateAPIView, UserProfileAPIView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path('register/', UserCreateAPIView.as_view(), name='user_register'),
-    path('profile/<int:pk>/', UserProfileAPIView.as_view(), name='user_profile'),
-    path('payments/', PaymentListAPIView.as_view(), name='payment_list'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("register/", UserCreateAPIView.as_view(), name="user_register"),
+    path("profile/<int:pk>/", UserProfileAPIView.as_view(), name="user_profile"),
+    path("payments/", PaymentListAPIView.as_view(), name="payment_list"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
